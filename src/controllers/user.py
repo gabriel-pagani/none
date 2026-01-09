961bf5e18e691f4bdf63c6d5d3301f719c96eca9
gabriel-pagani <gabrielpaganidesouza@gmail.com>
2026-01-08 23:04:18 -0300

    Refactor User class: update get method to retrieve user by id and remove unused methods

--- src/controllers/user.py
+++ src/controllers/user.py
@@ -1,7 +1,6 @@
 from typing import Optional
 from os import urandom
 from database.connection import execute_query
-from utils.cryptor import generate_hash, verify_hash
 
 
 class User:
@@ -18,56 +17,22 @@ class User:
         self.master_password_hash = master_password_hash
 
     @classmethod
-    def create(cls, username: str, master_password: str) -> Optional['User']:
-        salt = urandom(32)
-        master_password_hash = generate_hash(master_password)
-
-        try:
-            execute_query(
-                query="INSERT INTO users (username, master_password_hash, salt) VALUES (?, ?, ?)",
-                params=(username, master_password_hash, salt)
-            )
-
-            return cls.get(username)
-
-        except Exception as e:
-            print(f"exception-on-create: {e}")
-            return None
-
-    @classmethod
-    def get(cls, username: str) -> Optional['User']:
+    def get(cls, id: int) -> Optional['User']:
         try:
             response = execute_query(
-                "SELECT id, salt, username, master_password_hash FROM users WHERE username = ?",
-                (username,)
+                "SELECT * FROM users WHERE id = ?",
+                (id,)
             )
             
             if response != []:
-                return cls(id=response[0][0], salt=response[0][1], username=response[0][2], master_password_hash=response[0][3])
+                return cls(
+                    id=response[0][0], 
+                    salt=response[0][1], 
+                    username=response[0][2], 
+                    master_password_hash=response[0][3]
+                )
             return None
 
         except Exception as e:
-            print(f"exception-on-read: {e}")
+            print(f"exception-on-get: {e}")
             return None
-
-    def delete(self) -> bool:
-        try:
-            if not self.id:
-                return False
-            
-            execute_query(
-                query="DELETE FROM users WHERE id = ?",
-                params=(self.id,)
-            )
-
-            return True
-
-        except Exception as e:
-            print(f"exception-on-delete: {e}")
-            return False
-        
-    def check_master_password(self, master_password: str) -> bool:
-        if not self.master_password_hash:
-            return False
-            
-        return verify_hash(self.master_password_hash, master_password)
